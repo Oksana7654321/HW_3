932e582c3d5f0272b27585f74b3583f741206f8b
Oksana Ohorilko <ksnohorilko@gmail.com>
2020-03-01 13:05:38 +0200

    Add files via upload

---
+++ HW_3_Task_2.py
@@ -0,0 +1,7 @@
+def reverse_words_in_string(text):
+    for line in text.split('\n'):
+        return(' '.join(line.split()[::-1]))
+        
+print(reverse_words_in_string('The Tests Given in the Instructions'))
+print(reverse_words_in_string('Hello World'))
+print(reverse_words_in_string('Hi There.'))
